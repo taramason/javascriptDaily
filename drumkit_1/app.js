6033a75167241420a1414827c28e6d31c049af55
Tara Mason <tara.mason@colostate.edu>
2019-05-06 14:06:59 -0600

    keys hooked up

--- drumkit_1/app.js
+++ drumkit_1/app.js
@@ -1,3 +1,11 @@
-window.addEventListener('keydown', function(e){
- console.log(e.keyCode);
-});
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return; //STOP FUNCTION
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing'); //add class for key animation
+}
+
+
